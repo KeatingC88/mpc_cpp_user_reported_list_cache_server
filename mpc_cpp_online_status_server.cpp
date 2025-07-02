9dc2d4622e7beda9bb9cf5f6f6ea75851683530f
keatingc88@gmail.com <keatingc88@gmail.com>
2025-07-01 22:29:25 -0500

    Add project files.

---
+++ mpc_cpp_online_status_server.cpp
@@ -0,0 +1,22 @@
+﻿#pragma warning(push)
+#pragma warning(disable: 4267 4244 4305) // size_t → int usigned, etc.
+
+#include "crow_all.h"
+
+#pragma warning(pop)
+
+#include <cpp_redis/core/client.hpp>
+#include <iostream>
+
+int main() 
+{
+
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/")([]() {
+        return "Hello, World from C++ Web API!";
+    });
+
+    app.bindaddr("127.0.0.1").port(3003).multithreaded().run();
+
+}
