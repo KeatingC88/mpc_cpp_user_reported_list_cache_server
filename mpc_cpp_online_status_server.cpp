f9915575e48292ba5fe2e47f3ed51dc393ec831c
keatingc88@gmail.com <keatingc88@gmail.com>
2025-07-13 01:48:51 -0500

    foundation set and must now implement AES and JWT

--- mpc_cpp_online_status_server.cpp
+++ mpc_cpp_online_status_server.cpp
@@ -1,18 +1,46 @@
-﻿#pragma warning(push)
-#pragma warning(disable: 4267 4244 4305) // size_t → int usigned, etc.
-#include "crow_all.h"
-#pragma warning(pop)
+﻿#include "crow_all.h"
+#include <cpp_redis/cpp_redis>
+#include <tacopie/tacopie>
+#include <string>
+
+
+cpp_redis::client client;
 
 int main() 
 {
-    crow::SimpleApp app;
+    client.connect("127.0.0.1", 20346, [](const std::string& host, std::size_t port, cpp_redis::client::connect_state status) {
+        if (status == cpp_redis::client::connect_state::dropped) {
+            std::cerr << "Client disconnected from " << host << ":" << port << std::endl;
+        }
+    });
 
-    CROW_ROUTE(app, "/")([]() {
-        return "Hello, World from C++ Web API!";
+    crow::SimpleApp app;
+    
+    CROW_ROUTE(app, "/set/<uint>/<int>")([](unsigned int user_id, int online_status) {
+
+        std::string user_id_string = std::to_string(user_id);
+        std::string online_status_string = std::to_string(online_status);
+
+        client.set(user_id_string, online_status_string);
+        client.sync_commit();
+        return crow::response(200, user_id_string + ":" + online_status_string);
+
     });
 
 
+    CROW_ROUTE(app, "/get/<uint>")([](unsigned int user_id) {
 
+        std::string user_id_string = std::to_string(user_id);
+
+        auto get_reply_future = client.get(user_id_string);
+
+        client.sync_commit();
+
+        cpp_redis::reply reply = get_reply_future.get();
+
+        return crow::response(200, reply.as_string());
+
+    });
 
 
     app.bindaddr("127.0.0.1").port(3003).multithreaded().run();
